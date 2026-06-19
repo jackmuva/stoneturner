e454f928d090f6ce75177b600ba4d2e04aa503c1
jackmuva <jackmu@umich.edu>
2026-06-19 14:40:15 -0700

    config for discord

--- src/integrations/discord/config.ts
+++ src/integrations/discord/config.ts
@@ -4,7 +4,7 @@ const urlParams = new URLSearchParams({
   response_type: "code",
   client_id: process.env.BUN_PUBLIC_DISCORD_CLIENT_ID ?? "",
   scope: process.env.BUN_PUBLIC_DISCORD_SCOPE ?? "",
-  redirect_uri: process.env.BUN_PUBLIC_DISCORD_REDIRECT_URI ?? "",
+  redirect_uri: `${process.env.BUN_PUBLIC_BACKEND_BASE_URL}/api/oauth/discord`,
   prompt: "consent",
   integration_type: "1",
 }).toString();
