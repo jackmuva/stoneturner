7e404c7690a20cead51702808eb51154fc348629
jackmuva <jackmu@umich.edu>
2026-07-04 13:12:20 -0700

    better colors

--- migrations/20260702193045_tiny_rage/migration.sql
+++
@@ -1,1 +0,0 @@
-CREATE INDEX `idx_md_artifacts_integration` ON `mdArtifacts` (`integration`);
--- migrations/20260702200908_safe_tarantula/migration.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS `idx_md_artifacts_integration`;
-CREATE INDEX `idx_md_artifacts_integration_date` ON `mdArtifacts` (`integration`,`artifactDate`);
